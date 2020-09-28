f75341e2fc308c91a71de50aa2cdc9fef1e0370c
David Toledo <dtoledoz0207@gmail.com>
2020-09-28 11:57:20 -0500

    create-sideMenu-with-Drawer-Widget-in-homePage

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:userpreferencesapp/src/pages/settings_page.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -9,6 +10,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('User preferences')),
+      drawer: _createSideMenu(context),
       body: Container(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -21,6 +23,52 @@ class HomePage extends StatelessWidget {
             Divider()
           ],
         ),
+      ),
+    );
+  }
+
+  Drawer _createSideMenu(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/menu-img.jpg'),
+                fit: BoxFit.cover
+              )
+            ),
+          ),
+
+          ListTile(
+            leading: Icon(Icons.home, color: Colors.blue),
+            title: Text('Home'),
+            onTap: () => Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+
+          ListTile(
+            leading: Icon(Icons.party_mode, color: Colors.blue),
+            title: Text('Party mode'),
+            onTap: () {},
+          ),
+
+          ListTile(
+            leading: Icon(Icons.people, color: Colors.blue),
+            title: Text('People'),
+            onTap: () {},
+          ),
+
+          ListTile(
+            leading: Icon(Icons.settings, color: Colors.blue),
+            title: Text('Settings'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, SettingsPage.routeName);
+            },
+          )
+
+        ],
       ),
     );
   }
