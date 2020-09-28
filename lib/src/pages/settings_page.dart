e3c7d68da9cfc496798a15ad9762e7c26f427724
David Toledo <dtoledoz0207@gmail.com>
2020-09-28 12:18:30 -0500

    Create independet Widget for SideMenu
    This creates an independent SideMenu Widget for Drawer Widget in
    the widgets folder.

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:userpreferencesapp/src/widgets/sidemenu_widget.dart';
 
 
 class SettingsPage extends StatelessWidget {
@@ -9,6 +10,7 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Settings')),
+      drawer: SideMenuWidget(),
       body: Center(child: Text('Settings page')),
     );
   }
